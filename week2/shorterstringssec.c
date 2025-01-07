e5e2c9c5933bd85676b674b5e373ec012c6d2453
AZahin13 <zahinozlan@hotmail.com>
2025-01-08 00:48:28 +0800

    Updated shorterstringssec.c
    integrated using command line arguments

--- week2/shorterstringssec.c
+++ week2/shorterstringssec.c
@@ -1,20 +1,42 @@
-//this is embarassing
+// using command line arguments
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    string letters = get_string("Letters: \n");
+    // get user's input from command line
+    string letters = argv[1];
+
+    // ask if no second arg
+    if (argc != 2)
+    {
+        printf("Please input a 2nd arg\n");
+        return 1;
+    }
+
+    // check if symbols present
+    for (int i = 0; i < strlen(letters); i++)
+    {
+        if (!(isalpha(letters[i])))
+        {
+            printf("Please exclude symbols\n");
+            return 2;
+        }
+    }
+    // string letters = get_string("Letters: \n");
+    // loop through the string
     for (int i = 0; i < strlen(letters) - 1; i++)
     {
-        if (letters[i+1] < letters[i])
+        // letters later in the alphabet have >value
+        if (letters[i + 1] < letters[i])
         {
-            printf("No\n");
+            printf("No, alphabetical order pls\n");
             return 0;
         }
     }
+    // if no letters > value than before
     printf("Yes\n");
     return 0;
 }
-
